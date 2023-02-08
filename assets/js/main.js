d030a5f7b4fb9e63a47355b6f3d108dfa0af5eb6
puneethkanna <neerukondapuneeth@gmail.com>
2023-02-09 00:27:31 +0530

    Changed About Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,45 @@
     el.addEventListener('scroll', listener)
   }
 
+
+  const statsStart = () => {
+        
+    var statSection = $(".about-stats"),
+        stats = $(".stats__count");
+
+    statSection.waypoint({
+
+        handler: function(direction) {
+
+            if (direction === "down") {
+
+                stats.each(function () {
+                    var $this = $(this);
+
+                    $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (curValue) {
+                            $this.text(Math.ceil(curValue));
+                        }
+                    });
+                });
+
+            } 
+
+            // trigger once only
+            this.destroy();
+
+        },
+
+        offset: "90%"
+
+    });
+}; 
+window.addEventListener('load', statsStart)
+onscroll(document, statsStart)
+
+
   /**
    * Navbar links active state on scroll
    */
